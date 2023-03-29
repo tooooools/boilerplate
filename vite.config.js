222bfa6a317b4cc51b74d8462bba4b0ac2692150
Arnaud Juracek <arnaud.juracek@gmail.com>
2023-03-29 23:27:28 +0200

    Allow setting base via .env files

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,56 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // If a backend is needed, see https://vitejs.dev/guide/backend-integration.html
 
-export default defineConfig(({ mode }) => ({
-  root: path.join(__dirname, 'src'),
-  publicDir: path.join(__dirname, 'public'),
-  envDir: path.join(__dirname),
-  build: { outDir: path.join(__dirname, 'build') },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-  define: {
-    __VERSION__: JSON.stringify(process.env.npm_package_version)
-  },
+  return {
+    root: path.join(__dirname, 'src'),
+    publicDir: path.join(__dirname, 'public'),
+    envDir: path.join(__dirname),
+    build: { outDir: path.join(__dirname, 'build') },
 
-  plugins: [
-    { // Replace index.jsx by test/test.jsx as entry in test mode
-      name: 'html-inject-test',
-      transformIndexHtml: html => mode === 'test' ? html.replace('/index.jsx', '/test/test.jsx') : html,
-      config: () => ({
-        resolve: {
-          alias: { '/test': path.join(__dirname, 'test') }
-        }
-      })
-    }
-  ],
+    base: env.BASE || '/',
 
-  server: {
-    port: 8080,
-    host: true
-  },
+    define: {
+      __VERSION__: JSON.stringify(process.env.npm_package_version)
+    },
 
-  css: {
-    devSourcemap: true,
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @use '/style/_helpers' as *;
-          @use '/style/_devices' as *;
-          $env: ${mode};
-        `
+    plugins: [
+      { // Replace index.jsx by test/test.jsx as entry in test mode
+        name: 'html-inject-test',
+        transformIndexHtml: html => mode === 'test' ? html.replace('/index.jsx', '/test/test.jsx') : html,
+        config: () => ({
+          resolve: {
+            alias: { '/test': path.join(__dirname, 'test') }
+          }
+        })
       }
-    }
-  },
+    ],
 
-  esbuild: {
-    jsxInject: "import h from '/utils/jsx/h'",
-    jsxFactory: 'h'
+    server: {
+      port: 8080,
+      host: true
+    },
+
+    css: {
+      devSourcemap: true,
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use '/style/_helpers' as *;
+            @use '/style/_devices' as *;
+            $env: ${mode};
+          `
+        }
+      }
+    },
+
+    esbuild: {
+      jsxInject: "import h from '/utils/jsx/h'",
+      jsxFactory: 'h'
+    }
   }
-}))
+})
